5a59ebfb3da79604d2fac3a98dcc63a81bba6575
Tatiana Davydiuk <tatianadavydiuk@gmail.com>
2017-08-30 14:55:58 +0300

    create controller session & page sign in & spec sign in

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "AuthenticationPages", type: :request do
+  describe "Authentication" do
+
+    subject { page }
+
+    describe "signin page" do
+      before { visit signin_path }
+
+      it { should have_content('Sign in') }
+      it { should have_title('Sign in')}
+    end
+  end
+end
