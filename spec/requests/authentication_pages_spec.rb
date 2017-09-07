e26882d43c3d4267a2358823811d203e75623239
Tatiana Davydiuk <tatianadavydiuk@gmail.com>
2017-09-07 18:06:14 +0300

    sign out & spec

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe "AuthenticationPages", type: :request do
     end
 
     describe "signin" do
+      let(:user) { FactoryGirl.create(:user) }
       before { visit signin_path }
 
       describe "with invalid information" do
@@ -28,16 +29,25 @@ RSpec.describe "AuthenticationPages", type: :request do
       end
 
       describe "with valid information" do
-        let(:user) { FactoryGirl.create(:user) }
+
         before do
           fill_in "Email", with: user.email.upcase
           fill_in "Password", with: user.password
           click_button "Sign in"
+          click_link "Account"
         end
 
         it { should have_title(user.name) }
+
+
         it { should have_link ('Profile') }
         it { should have_link ('Sign out') }
+
+        describe "followed by signout" do
+          before { click_link "Sign out" }
+          it { should have_link('Sign in') }
+        end
+
       end
 
     end
